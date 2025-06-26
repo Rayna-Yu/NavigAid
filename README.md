697f8ea70c426e68807356be0f6844fe2c8cdc55
Rayna-Yu <Alinasharon10@gmail.com>
2025-06-26 10:00:45 -0400

    plain style (update later)

--- README.md
+++ README.md
@@ -6,7 +6,18 @@
 ```bash
 cd NavigAid
 npm install
+```
+
+## Install dependencies
+```bash
+npx expo install react-native maps
+npx expo install @expo/vector-icons
+```
+
+## Try it out
+```bash
 npx expo start
 ```
+
 Make sure you have expo go installed on your phone and scan the QR.
 You should now see the app on your phone
--- app/frontend/main.tsx
+++ app/frontend/main.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput} from 'react-native';
+import MapView from 'react-native-maps';
 import { StatusBar } from 'expo-status-bar';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function Main() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>NavigAid</Text>
-      <StatusBar style="light" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 42.3624,
+          longitude: -71.0871,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+          // TODO : set to user's initial location not hard code it
+        }}
+      />
+      <View style={styles.searchBox}>
+        <Text style={styles.label}>From:</Text>
+        <TextInput style={styles.input} placeholder="Starting point" />
+        <AntDesign name="arrowdown" size={24} color="black" style={styles.arrow} />
+        <Text style={styles.label}>To:</Text>
+        <TextInput style={styles.input} placeholder="Destination" />
+      </View>
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -14,15 +32,35 @@ export default function Main() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1e3c72',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 42,
-    fontWeight: 'bold',
-    color: '#ffffff',
-    letterSpacing: 2,
-    textTransform: 'uppercase',
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  searchBox: {
+    position: 'absolute',
+    top: 60,
+    left: 20,
+    right: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 16,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  label: {
+    fontWeight: '600',
+    marginBottom: 4,
+    fontSize: 18,
+  },
+  input: {
+    backgroundColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 12,
+  },
+  arrow: {
+    alignSelf: 'center',
   },
 });
